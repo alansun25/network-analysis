07bb0db1975a4dc9e806328a4da33ef154a93c0a
Alan Sun <mralansun@gmail.com>
2021-06-22 22:39:27 -0400

    Fixed a typo

--- functions/random_graph.R
+++ functions/random_graph.R
@@ -2,7 +2,7 @@ library(igraph)
 
 # Random Graph Generation
 #
-# Generates a random, undirected graph
+# Generates a random, undirected graph.
 # Parameters: 
 #   n (number of nodes)
 #   p (probability of a link between any two nodes; between 0 and 1)
@@ -14,7 +14,7 @@ random_graph <- function(n, p) {
     for (j in 1:n) {
       rand <- runif(1)
       # If the random number is less than the probability of a link
-      # between nodes i and j and i and j are not alreadt connected,
+      # between nodes i and j and i and j are not already connected,
       # create an edge between i and j.
       if (rand <= p && !are.connected(g, i, j)) {
         g <- add_edges(g, c(i,j))
