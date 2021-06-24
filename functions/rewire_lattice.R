9e1dc5baae147b7cf3182001b3d9c1ce5b39a478
Alan Sun <mralansun@gmail.com>
2021-06-24 13:00:53 -0400

    Changed edge deletions from edge i to edge 1 in each iteration of the loop

--- functions/rewire_lattice.R
+++ functions/rewire_lattice.R
@@ -23,26 +23,22 @@ rewire_lattice <- function(n, k, p) {
   }
   
   if (p < 0 || p > 1) {
-    stop("Probability value invalid.")
+    stop("Probability value must be between 0 and 1.")
   }
   
   g <- make_lattice(length = n, dim = 1, nei = k, circular = TRUE)
   size <- gsize(g)
   
-  # I think I may to do need this, but not too sure... was running into problems of using an edge set of a
-  # different graph
-  # Duplicate initial lattice so we only change the duplicate and can still access the original edges
-  # tg <- duplicate(g)
-  
+  # Note: We delete edge 1 each time, which means what was edge 2 before is now edge 1. The for-loop
+  # ensures we do this until all initial edges have been rewired.
   for(i in 1:size) {
     edges <- E(g)
     
-    # Randomly choose one of the nodes incident on edge i to be the starting node
-    start <- ends(g, edges[i])[sample(1:2, 1)]
+    # Randomly choose one of the nodes incident on the initial edge to be the starting node
+    start <- ends(g, edges[1])[sample(1:2, 1)]
 
     prob <- runif(1)
     if (prob < p) {
-      g <- delete_edges(g, edges[i]) # Remove initial edge
       rewire <- sample(1:n, 1) # Choose random node to rewire to
       
       # No self-loops or multiple edges
@@ -50,6 +46,7 @@ rewire_lattice <- function(n, k, p) {
         rewire <- sample(1:n, 1)
       }
       
+      g <- delete_edges(g, edges[1]) # Remove initial edge
       g <- add_edges(g, c(start, rewire)) # Rewire the edge
     }
   }
@@ -57,11 +54,6 @@ rewire_lattice <- function(n, k, p) {
   return(g)
 }
 
-rewired <- rewire_lattice(20, 2, 0.4)
+rewired <- rewire_lattice(10, 2, 0.345)
 gsize(rewired)
 plot(rewired, layout=layout_in_circle(rewired))
-
-# tt <- make_lattice(length = 3, dim = 1, nei = 2, circular = TRUE)
-# E(tt)
-# tt <- delete_edges(tt, E(tt)[1])
-# E(tt)
