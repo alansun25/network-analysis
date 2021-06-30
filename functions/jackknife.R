decc8cc8b16feb67b7624d1e327ae245a9cfd630
Alan Sun <mralansun@gmail.com>
2021-06-30 14:30:16 -0400

    Jackknife function; some file management.

---
+++ functions/jackknife.R
@@ -0,0 +1,38 @@
+library(igraph)
+
+# Vertex Jackknife
+
+jackknife <- function(g, stat) {
+  stat <- tolower(stat)
+  
+  if (!is_igraph(g)) {
+    stop("Object must be an igraph graph.")
+  }
+  
+  if (stat != "density" && stat != "transitivity") {
+    stop("Provided statistic not currently supported")
+  }
+  
+  n <- vcount(g)
+  jk_stats <- c()
+  
+  for(i in 1:n) {
+    jg <- delete_vertices(g, V(g)[i])
+    if (stat == "density") {
+      jk_stats <- append(jk_stats, edge_density(jg))
+    } else {
+      jk_stats <- append(jk_stats, transitivity(jg, type="global"))
+    }
+  }
+  
+  return(jk_stats)
+}
+
+s <- make_star(30, "undirected")
+plot(s)
+(jackknife(s, "density"))
+
+t <- make_lattice(length=12, dim=1, nei = 2)
+plot(t)
+(jackknife(t, "density"))
+(jackknife(t, "transitivity"))
--- functions/rewire_lattice.R
+++ functions/rewire_lattice.R
@@ -13,6 +13,12 @@ library(igraph)
 # The point of interest here is to rewire the lattice to have low average path length (small world)
 # as well as high clustering.
 rewire_lattice <- function(n, k, p) {
+  l <- make_lattice(length = n, dim = 1, nei = k, circular = TRUE)
+  
+  if (k == n/2) {
+    return(l)
+  }
+  
   if (n < 0) {
     stop("Graph cannot have a negative number of nodes.")
   }
@@ -21,11 +27,14 @@ rewire_lattice <- function(n, k, p) {
     stop("Neighbors cannot have negative distance.")
   }
   
+  if (k > n/2) {
+    stop("Neighborhood is not well defined.")
+  }
+  
   if (p < 0 || p > 1) {
     stop("Probability value must be between 0 and 1.")
   }
-  
-  l <- make_lattice(length = n, dim = 1, nei = k, circular = TRUE)
+
   e <- make_empty_graph(n, directed = FALSE)
 
   edges_visited <- make_empty_graph(n, directed=FALSE)
@@ -38,17 +47,20 @@ rewire_lattice <- function(n, k, p) {
       v1 <- ends(l, n_edges[j])[1]
       v2 <- ends(l, n_edges[j])[2]
       
+      start <- c(v1, v2)[sample(1:2, 1)]
+      
       # Only rewire if probability p is exceeded and if edge j has not already been visited.
       if (!are_adjacent(edges_visited, v1, v2)) {
         if (prob < p) {
           rewire <- sample(1:n, 1) # Choose random node to rewire to
           
           # No self-loops
-          while (rewire == i) {
+          while (rewire == start || are_adjacent(e, rewire, start)) {
+            print(edge_density(e))
             rewire <- sample(1:n, 1)
           }
           
-          e <- add_edges(e, c(i, rewire))
+          e <- add_edges(e, c(start, rewire))
         } else {
           if (!are_adjacent(e, v1, v2)) {
             e <- add_edges(e, c(v1, v2)) 
@@ -59,13 +71,10 @@ rewire_lattice <- function(n, k, p) {
       }
     }
   }
-  
-  # No multiple-edges (may cause the number of total edges in the graph to decrease)
-  e <- simplify(e)
 
   return(e)
 }
 
-rewired <- rewire_lattice(8, 2, 0.5)
+rewired <- rewire_lattice(10, 4, 1)
 gsize(rewired)
 plot(rewired, layout=layout_in_circle(rewired))
