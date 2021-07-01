44a1661d90962c2fda554161fdc6a95c8822caf9
Elizabeth Upton <32308172+elizabeth-upton@users.noreply.github.com>
2021-07-01 11:45:27 -0400

    allows self loops...but not multiple edges.  Need to add additional checks if want to remove loops

--- functions/Liz/watts_strogatz.R
+++ functions/Liz/watts_strogatz.R
@@ -5,8 +5,8 @@ w_strogatz <- function(n, k, p) {
     stop("Graph cannot have a negative number of nodes.")
   }
   
-  if (k > n/2) {
-    stop("Invalid node neighbor distance.")
+  if (k >= n/2) {
+    stop("Invalid node neighbor distance; this is a complete graph.")
   }
   
   if (p < 0 || p > 1) {
@@ -17,17 +17,41 @@ w_strogatz <- function(n, k, p) {
   
   for(i in 1:n) {
     for(j in (i + 1) : (i + k)){
-      j <- (j - 1) %% n + 1
-      prob <- runif(1)
+     
+       j <- (j - 1) %% n + 1 #reindex j 
+      prob <- runif(1) #rewire?
+      head <- runif(1) #rewire head or tail?
 
-      if (prob < p) {
-        rewire <- sample(1:n, 1)
-        #check that the edge is new.  Need to update this. 
-        #don't allow self-loops
-        #how to add back? 
-        g_new <- g_new + edge(c(i, rewire)) #add edges between the node
-      } else {
+      if (prob < p || are_adjacent(g_new, i, j)) { #rewire given rewire probability OR if the non-rewired edge already exists due to prior rewiring
+        if(head > .5) { #i is head
+          
+          tag <- neighbors(g_new,i) #capturing edges that already exist
+          
+          if (length (tag) == 0){
+            rewire <- sample(V(g_new)[-c(i,j)], 1) #catch no neighbors, choose a random node to attach 
+          } else {
+          rewire <- sample(V(g_new)[-tag], 1) #sample from non neighbors
+          }
+          
+          g_new <- g_new + edge(i, rewire)
+          
+        } else { #j is head
+          
+          tag <- neighbors(g_new,j)
+          
+          if (length (tag) == 0){
+            rewire <- sample(V(g_new)[-c(i,j)], 1)
+          } else {
+          rewire <- sample(V(g_new)[-tag], 1) #sample from non neighbors
+          }
+          
+          g_new <- g_new + edge(j, rewire)
+        }
+      
+        } else {
+        
         g_new <- g_new + edge(c(i,j))
+        
       }
     }
   }
@@ -35,5 +59,7 @@ w_strogatz <- function(n, k, p) {
   return(g_new)
 }  
   
-g_new <- w_strogatz(n = 10, k =1, p =1)
+g_new <- w_strogatz(n = 10, k =4, p =.9)
 plot(g_new, layout = layout_in_circle(g_new))
+
+
